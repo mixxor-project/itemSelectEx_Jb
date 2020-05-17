00775625cd850c995b679d6f2364200d140df0bf
mixxor-project <mixxorproject@gmail.com>
2020-05-18 02:55:36 +0900

    버튼 선택에 따라 scene 전환 다름

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 public class ChangeScene : MonoBehaviour
 {
-    public void changeScene()
+    string btnName = "";
+    public void CorrectButton()
     {
-        SceneManager.LoadScene("LoadScene");
+        //correct = true;
+        SceneManager.LoadScene("CorrectScene");
+    }
+    public void WrongButton()
+    {
+        SceneManager.LoadScene("WrongScene");
     }
     void Start()
     {
-        changeScene();
+        GameObject tempBtn = EventSystem.current.currentSelectedGameObject;
+        if (tempBtn)
+        {
+            btnName = tempBtn.name;
+            if (btnName.Equals("wrong"))
+            {
+                CorrectButton();
+            }
+            else if (btnName.Equals("correct"))
+            {
+                WrongButton();
+            }
+        }
+        else
+        {
+            Debug.Log("No game object called wibble found");
+        }
     }
     void Update()
     {
-        
+
     }
 }
